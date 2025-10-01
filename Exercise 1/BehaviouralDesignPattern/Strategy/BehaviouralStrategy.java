0f9e9aa9c377e1244c531f7eeb96ffa39dcd65b3
keerthi-07-git <skeerthilakshmi07@gmail.com>
2025-10-01 18:55:06 +0530

    Update BehaviouralStrategy.java

--- Exercise 1/BehaviouralDesignPattern/Strategy/BehaviouralStrategy.java
+++ Exercise 1/BehaviouralDesignPattern/Strategy/BehaviouralStrategy.java
@@ -1,14 +1,18 @@
 package behaviouraldesignpattern.strategy.src;
-
 public class BehaviouralStrategy {
     public static void main(String[] args) {
-        int[] nums = {3, 1, 2};
-        Sorter s = new Sorter();
+        int[] numbers = {5, 2, 9, 1, 7};
 
-        s.setStrategy(new QuickSort());
-        s.sort(nums);
+        Sorter sorter = new Sorter();
 
-        s.setStrategy(new MergeSort());
-        s.sort(nums);
+        // Use QuickSort strategy
+        sorter.setStrategy(new QuickSort());
+        sorter.sortArray(numbers.clone());  // clone to keep original array
+
+        // Switch to MergeSort strategy
+        sorter.setStrategy(new MergeSort());
+        sorter.sortArray(numbers.clone());
     }
 }
+
+
