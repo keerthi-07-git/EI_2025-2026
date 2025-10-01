37f952d92b136fc3143ba10939715b3de7f3f219
keerthi-07-git <skeerthilakshmi07@gmail.com>
2025-10-01 13:41:10 +0530

    Delete Exercise 2/Satellite Command System directory

--- Exercise 2/Satellite Command System/command/Command.java
+++
@@ -1,9 +0,0 @@
-package com.satellite.command.command;
-
-import com.satellite.command.controller.SatelliteController;
-
-public interface Command {
-    CommandResult execute(SatelliteController controller);
-    String getCommandName();
-    String getDescription();
-}                            
--- Exercise 2/Satellite Command System/exception/InvalidOrientationException.java
+++
@@ -1,9 +0,0 @@
-package com.satellite.command.exception;
-
-public class InvalidOrientationException extends SatelliteException {
-    private static final long serialVersionUID = 1L;
-
-    public InvalidOrientationException(String message) {
-        super(message);
-    }
-}
