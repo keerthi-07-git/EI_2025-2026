a2c829883d41a91a5b56773689ebb35f3765d4c0
keerthi-07-git <skeerthilakshmi07@gmail.com>
2025-10-01 18:54:39 +0530

    Update Sorter.java

--- Exercise 1/BehaviouralDesignPattern/Strategy/src/Sorter.java
+++ Exercise 1/BehaviouralDesignPattern/Strategy/src/Sorter.java
@@ -3,11 +3,14 @@ package behaviouraldesignpattern.strategy.src;
 public class Sorter {
     private SortStrategy strategy;
 
-    public void setStrategy(SortStrategy s) {
-        strategy = s;
+    public void setStrategy(SortStrategy strategy) {
+        this.strategy = strategy;
     }
 
-    public void sort(int[] arr) {
+    public void sortArray(int[] arr) {
+        if (strategy == null) {
+            throw new IllegalStateException("No strategy set!");
+        }
         strategy.sort(arr);
     }
 }
