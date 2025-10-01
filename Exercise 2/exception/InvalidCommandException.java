62897fdf7cd1b10c682a3f7eb5bc2b1559edbb64
keerthi-07-git <skeerthilakshmi07@gmail.com>
2025-10-01 13:36:42 +0530

    Delete Exercise 2 directory

--- Exercise 2/exception/InvalidCommandException.java
+++
@@ -1,9 +0,0 @@
-package com.satellite.command.exception;
-
-public class InvalidCommandException extends SatelliteException {
-    private static final long serialVersionUID = 1L;
-
-    public InvalidCommandException(String message) {
-        super(message);
-    }
-}
